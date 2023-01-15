bf8c52df73924ae5918f94fc43cc1d915e409635
DominickRobinson <72145105+DominickRobinson@users.noreply.github.com>
2023-01-15 15:09:48 -0500

    fix 3

--- Scenes/UI/Lose.gd
+++ Scenes/UI/Lose.gd
@@ -3,11 +3,12 @@ extends Control
 
 onready var submit_button = $PausePanel/CenterContainer/VBoxContainer/SubmitScore
 
-
+onready var Name = $PausePanel/CenterContainer/VBoxContainer/HBoxContainer/Name
 
 
 func _on_SubmitScore_pressed():
-	if GameManager.player_name != "":
+	if Name.text != "":
+		GameManager.player_name = Name.text
 		GameManager.upload_score()
 		submit_button.disabled = true
 	else:
